e13ba6d665cd08a2b6795b5df7029f938c3b521b
taylorjackson <taylor.jackson@me.com>
2023-10-27 17:50:58 -0700

    cloudflare

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,7 +2,6 @@
 
 import { createContext, useEffect, useRef } from 'react'
 import { usePathname } from 'next/navigation'
-import { ThemeProvider, useTheme } from 'next-themes'
 
 function usePrevious<T>(value: T) {
   let ref = useRef<T>()
@@ -14,30 +13,6 @@ function usePrevious<T>(value: T) {
   return ref.current
 }
 
-function ThemeWatcher() {
-  let { resolvedTheme, setTheme } = useTheme()
-
-  useEffect(() => {
-    let media = window.matchMedia('(prefers-color-scheme: dark)')
-
-    function onMediaChange() {
-      let systemTheme = media.matches ? 'dark' : 'light'
-      if (resolvedTheme === systemTheme) {
-        setTheme('system')
-      }
-    }
-
-    onMediaChange()
-    media.addEventListener('change', onMediaChange)
-
-    return () => {
-      media.removeEventListener('change', onMediaChange)
-    }
-  }, [resolvedTheme, setTheme])
-
-  return null
-}
-
 export const AppContext = createContext<{ previousPathname?: string }>({})
 
 export function Providers({ children }: { children: React.ReactNode }) {
@@ -46,10 +21,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <AppContext.Provider value={{ previousPathname }}>
-      <ThemeProvider attribute="class" disableTransitionOnChange>
-        <ThemeWatcher />
-        {children}
-      </ThemeProvider>
+      {children}
     </AppContext.Provider>
   )
 }
